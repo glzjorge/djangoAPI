1395315821246b975a36e69456420f605d34ce12
Jorge Luis Gonzalez Hernandez <jorgegh@delicias.tecnm.mx>
2023-06-22 13:44:35 -0600

    obtener datos del usuario logeado reparado

--- blog/users/api/router.py
+++ blog/users/api/router.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('auth/register', RegisterView.as_view()),
     path('auth/login', TokenObtainPairView.as_view()),
     path('auth/token/refresh', TokenRefreshView.as_view()),
-    path('auth/me', UserView.as_view)
+    path('auth/me', UserView.as_view())
 ]
