03a27a8d71208cfcf830363444c1265990989d18
Jorge Luis Gonzalez Hernandez <jorgegh@delicias.tecnm.mx>
2023-06-22 13:52:24 -0600

    bug arreglado en obtener datos del usuario logeado

--- blog/users/api/views.py
+++ blog/users/api/views.py
@@ -2,9 +2,9 @@
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated, UserSerializer
+from rest_framework.permissions import IsAuthenticated
 #from users.models import User
-from users.api.serializers import UserRegisterSerializer
+from users.api.serializers import UserRegisterSerializer,  UserSerializer
 
 class RegisterView(APIView):
     def post(self, request):
